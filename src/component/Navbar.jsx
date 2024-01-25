b480de7465af7f350b5b1d2354caf55bf908f2f1
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2024-01-25 10:18:40 -0800

    'working on animation'

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import * as Unicons from '@iconscout/react-unicons';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import { UilAlignJustify } from '@iconscout/react-unicons'
+import { motion } from "framer-motion"
+import Animation from './Animation';
 
 const Navbar = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
@@ -10,15 +12,38 @@ const Navbar = () => {
     setDropdownOpen(!isDropdownOpen);
   };
 
+  const icon = {
+    hidden: {
+      pathLength: 0,
+      fill: "rgba(255, 255, 255, 0)"
+    },
+    visible: {
+      pathLength: 1,
+      fill: "rgba(255, 255, 255, 1)"
+    }
+  }
+
   return (
     <nav className="flex items-center justify-between  bg-secondary text-white p-9">
+       <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 100 100"
+  >
+    <motion.path
+      d="M0 100V0l50 50 50-50v100L75 75l-25 25-25-25z"
+      variants={icon}
+      initial="hidden"
+      animate="visible"
+    />
+  </svg>
+
             <div className='ml-[5%] text-[2.5vw] text-[white] font-bold'>Mj <span className='text-4xl text-primary'>.</span> </div>
       <ul className="hidden sm:flex space-x-4 ml-[50%] text-[1.4vw] text-[white] font-bold">
         <li className="hover:border-b-2 hover:border-primary  cursor-pointer">Home</li>
         <li className="hover:border-b-2 hover:border-primary cursor-pointer">About</li>
         <li className="hover:border-b-2 hover:border-primary cursor-pointer">Services</li>
         <li className="hover:border-b-2 hover:border-primary cursor-pointer">Contact</li>
-        {/* <Unicons.UilReact /> */}        
+        {/* <Unicons.UilReact /> */}    
       </ul>
       <div className="relative">
         {/* <button
--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import * as Unicons from '@iconscout/react-unicons';
+import {UilLinkedin, UilFacebook, UilInstagram  } from '@iconscout/react-unicons'
+
 
 const Footer = () => {
   return (
-    <div className='bg-slate-700 h-[80px]'>
-        <div className=' border-t-4 border-primary'>
-            <p className='mt-6 text-center text-[white]'>
+    <div className='bg-secondary h-[80px] flex justify-between'>
+           <div className='flex gap-3 mt-4'>
+          <div className='border border-[white] h-[40px] w-[40px] rounded-[30px] p-[6px]'><Unicons.UilLinkedin color='white' size='26px' /></div>
+          <div className='border border-[white] h-[40px] w-[40px] rounded-[30px] p-[6px]'><Unicons.UilFacebook color='white' size='26px' /></div>
+          <div className='border border-[white] h-[40px] w-[40px] rounded-[30px] p-[6px]'><Unicons.UilInstagram color='white' size='26px' /></div>
+        </div>
+        <div className=' '>
+            <p className='mt-6  text-[white]'>
             Copyright - Tortoiz | All Rights Reserved
             </p>
         </div>
