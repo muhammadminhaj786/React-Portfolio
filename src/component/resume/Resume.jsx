3946d3afb3ce6ef429afe1fffa8c3e64b79420aa
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2024-02-05 23:02:47 -0800

    'add rsume page'

---
+++ src/component/resume/Resume.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+// import { client, urlFor } from "../../client";
+import images from '../../assets/_MG_4156.JPG'
+
+
+const Resume = () => {
+  const [abouts, setAbouts] = useState([]);
+  const [aboutMe, setAboutMe] = useState({});
+
+  useEffect(() => {
+    const aboutsQuery = '*[_type == "abouts"]';
+    const aboutMeQuery = `*[_type == "aboutme"][0]{
+      profileImg,
+     description,
+     "resumeUrl": resume.asset -> url 
+    }`;
+    client.fetch(aboutsQuery).then((data) => {
+      setAbouts(data);
+    });
+    client.fetch(aboutMeQuery).then((data) => {
+      setAboutMe(data);
+    });
+  }, []);
+
+  const viewResumeHandler = () => {
+    window.open(aboutMe.resumeUrl, "_blank");
+  };
+
+  return (
+    <>
+      <h2 className="head-text">
+        I know that <span>Good Design</span>
+        <br />
+        means <span>Good Business</span>
+      </h2>
+
+      <div className="app__about-context app__flex">
+        <div className="app__about-img app__flex">
+          <div className="app__flex">
+            <img
+              src={
+                aboutMe.profileImg
+                  ? urlFor(aboutMe.profileImg)
+                  : images
+              }
+              alt="Profile"
+            />
+          </div>
+        </div>
+        <div className="app__about-data app__flex">
+          <h2 className="head-text">About Me</h2>
+          <p
+            className="p-text"
+            dangerouslySetInnerHTML={{ __html: aboutMe.description }}
+          ></p>
+          <div>
+            <button className="portfolio-button" onClick={viewResumeHandler}>
+              Resume
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 1.1 }}
+            transition={{ duration: 0.2, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: "20px" }}>
+              {about.title}
+            </h2>
+            <h2 className="p-text" style={{ marginTop: "10px" }}>
+              {about.description}
+            </h2>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Resume
